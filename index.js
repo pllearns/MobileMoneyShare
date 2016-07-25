465d3596e8578b71ac3e5b64c48bfd87d45f683a
nicosesma <nicosm310@gmail.com>
2016-07-25 12:05:13 -0700

    setup initial node, jade app

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+
+var app = express()
+var port = process.env.PORT || 3000
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.render('index', {});
+})
+
+app.get('/funds/:fundSlug', function(req, res){
+  var fundSlug = req.params.fundSlug
+
+  // TODO get this from the Database
+  var fund = {
+    id: 456,
+    slug: fundSlug,
+    name: 'Sally Needs A Laptop',
+    cycleStartDate: new Date,
+    cycleAmount: '20000', // cents ($200)
+    cycleLength: 7, // days
+  };
+
+  res.render('funds/show', {
+    fund: fund
+  });
+})
+
+app.listen(port)
+console.log('server started at http://0.0.0.0:'+port)
