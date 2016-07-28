33229ad12033e3c871e45fee355198758c855c73
Phillip Lorenzo <pllearns@gmail.com>
2016-07-28 15:09:49 -0700

    added text for email and made some changes to code

--- database.js
+++ database.js
@@ -14,6 +14,56 @@ var defineModels = function (db, models, next) {
   models.Invite = require('./models/invite')(db, models)
   models.FundMembership = require('./models/fundMembership')(db, models)
 
+  // models.Fund.find({}, function(error, funds){
+  //   if (error) return console.error(error)
+  //   var fund = funds[0]
+  //   inspectModelInstance(fund)
+  //
+  //   // user.getFundMemberships(function(error, fundMemberships){
+  //   //   console.log('fundMemberships', fundMemberships)
+  //   //   fundMemberships.forEach(inspectModelInstance)
+  //   // })
+  // })
+  //
+  // var invites = ['a','b','c'].map(function(x){
+  //   return new models.Invite({
+  //     name: x+' Smith',
+  //     email: x+'@example.com',
+  //     code: x+x+x+x+x,
+  //   })
+  // })
+  //
+  // var fund = new models.Fund({
+  //   name: 'test fund',
+  //   cycleStartDate: new Date(Date.parse('01/01/2018')),
+  //   paymentCycleLength: 1,
+  //   paymentAmount: 1,
+  //   invites: invites
+  // })
+  //
+  // fund.save(function(error){
+  //   if (error) throw error;
+  //
+  //   models.Invite.find({}, function(error, allInvites){
+  //     console.log('allInvites.length', allInvites.length)
+  //
+  //
+  //   })
+  //
+  //   // inspectModelInstance(models.Fund)
+  //
+  //
+  //   models.Fund.get(fund.id, function(error, fund){
+  //     if (error) throw error;
+  //     fund.getInvites(function(error){
+  //       if (error) throw error;
+  //       inspectModelInstance(fund)
+  //     })
+  //   })
+  //
+  // })
+
+
   db.sync(function(err) {
     if (err) throw err;
     next();
@@ -45,6 +95,18 @@ module.exports = {
 
 
 
-
-
 // module.exports = db
+
+
+
+function inspectModelInstance(model){
+  // console.log('model properties', Object.getOwnPropertyNames(model))
+  console.log(model.constructor.name, model.id)
+  var attributes = {}
+  for(var k in model) attributes[k] = model[k]
+  console.log('attributes', attributes)
+  Object.getOwnPropertyNames(model).forEach(function(k){
+    var value = typeof model[k] === 'function' ? '[Function]' : model[k]
+    console.log('model['+k+'] = ', value)
+  })
+}
